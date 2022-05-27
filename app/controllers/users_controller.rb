3e9808ad5baba1103f7a58ca91b3703c47aa8e46
Shinichiro28 <s.erk.ikt@gmail.com>
2022-05-27 13:48:41 +0000

    [add]日付設定による投稿数検索機能追加（非同期化）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      created_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{created_at}"]).count
+    end
+  end
+
   private
 
   def user_params
